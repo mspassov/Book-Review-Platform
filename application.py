bca3a06c2665f6a52257a7f653fa2c3c0e84e31f
Martin Spassov <martispassov@gmail.com>
2020-05-06 21:57:40 -0400

    skeleton website setup + html/css files templated

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
+import requests
 
-from flask import Flask, session
+from flask import Flask, session, render_template, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,4 +24,4 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html", title = "Home")
